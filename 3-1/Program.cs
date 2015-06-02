85709f5df4b061691a57e28a54e6b0721a2b200d
MarthaLRigby <martha.l.rigby@gmail.com>
2015-06-02 19:37:12 +0100

    Exercise 3-1

---
+++ 3-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 42;
+            float f = 98.6F;
+            double d = 12345.6789;
+            char c = 'z';
+            string s = "The quick brown fox jumped over the lazy dogs.";
+            Console.WriteLine(i + " " + f + " " + d + " " + c + " " + s);
+        }
+    }
+}
